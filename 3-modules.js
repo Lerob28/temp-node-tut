6fd532af064c87fb4c3a7eac5f977d86bf1a0843
Lerob28 <njeunkweborel@gmail.com>
2022-06-28 08:44:35 +0100

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+//  CommonJs, every file is module (by default) 
+//  Modules - Encapsuled Code (only share minimun)
+
+// const {john, peter} = require('./4-names')
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+
+const data = require('./6-alternative-flavor')
+require(`./7-mind-grenade`)
+sayHi(`susan`)
+sayHi(names.john)
+sayHi(names.peter)
